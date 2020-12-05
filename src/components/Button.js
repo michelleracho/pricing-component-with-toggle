83e5f582b411476738ba2a4c2e3f346004317941
Michelle Racho <rachoma12@gmail.com>
2020-12-04 23:53:16 -0500

    add and style button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import styles from '../styles/ButtonStyles.module.css';
+
 const Button = () => {
-  return <button>button</button>;
+  return <button className={styles.Button}>Learn more</button>;
 };
 
 export default Button;
